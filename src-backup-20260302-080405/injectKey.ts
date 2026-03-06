f411d94a6125ac43f0cb0145b083111cf0fc37aa
Nohkeyra <122115637+Nohkeyra@users.noreply.github.com>
2026-03-06 16:07:12 +0800

    Delete src-backup-20260302-080405 directory

--- src-backup-20260302-080405/injectKey.ts
+++
@@ -1,7 +0,0 @@
-
-// This script is used to inject the NVIDIA API key into localStorage
-(function() {
-  const key = "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";
-  localStorage.setItem('nvidiaApiKey', key);
-  console.log('NVIDIA API Key injected successfully.');
-})();
